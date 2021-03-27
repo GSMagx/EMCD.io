feb426a97eb7828c839babfac19e5dc20dfa9277
Alex <gsxmagx@me.com>
2021-03-27 17:28:54 +0100

    Empty App

---
+++ Shared/EMCD_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMCD_ioApp.swift
+//  Shared
+//
+//  Created by ALEX on 27.03.21.
+//
+
+import SwiftUI
+
+@main
+struct EMCD_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
