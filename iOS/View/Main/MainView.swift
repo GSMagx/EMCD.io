e0c643652f67de492da8f7cb301658d8338b5262
Alex <gsxmagx@me.com>
2021-03-28 10:28:53 +0200

    add Folders

---
+++ iOS/View/Main/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  EMCD.io
+//
+//  Created by ALEX on 27.03.21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        
+        
+        ZStack {
+            Text("Test")
+                
+        }
+            
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
